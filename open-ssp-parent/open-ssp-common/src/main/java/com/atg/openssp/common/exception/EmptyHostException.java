5b2f2a1321ee893857866f5f349df67a04aedb15
schmer <schmer@ad-tech-group.com>
2017-05-05 15:10:54 +0200

    New type of Exception.

---
+++ open-ssp-parent/open-ssp-common/src/main/java/com/atg/openssp/common/exception/EmptyHostException.java
@@ -0,0 +1,15 @@
+package com.atg.openssp.common.exception;
+
+/**
+ * @author André Schmer
+ *
+ */
+public class EmptyHostException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public EmptyHostException(final String msg) {
+		super(msg);
+	}
+
+}
