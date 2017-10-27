2c4d6070ff8dc1657cbd471815e8ef2afd824fe5
schmer <schmer@ad-tech-group.com>
2017-10-27 15:47:46 +0200

    Reduces parameter validation to simple siteid (datastructure example).

--- open-ssp-parent/core/src/main/java/com/atg/openssp/core/entry/EntryValidator.java
+++ open-ssp-parent/core/src/main/java/com/atg/openssp/core/entry/EntryValidator.java
@@ -1,22 +1,11 @@
 package com.atg.openssp.core.entry;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.atg.openssp.common.demand.ParamValue;
-import com.atg.openssp.common.exception.ERROR_CODE;
 import com.atg.openssp.common.exception.EmptyCacheException;
 import com.atg.openssp.common.exception.RequestException;
-import com.atg.openssp.core.cache.type.VideoAdDataCache;
-import com.atg.openssp.core.cache.type.WebsiteDataCache;
-import com.atg.openssp.core.cache.type.ZoneDataCache;
-
-import openrtb.tables.VideoBidResponseProtocol;
+import com.atg.openssp.core.cache.type.SiteDataCache;
 
 /**
  * The kind and diversity of request parameters my be vary and depends on the different conditions. See also at {@link ParamValue} for the kind aof params.
@@ -42,77 +31,61 @@ public class EntryValidator {
 
 		final ParamValue pm = new ParamValue();
 
+		// geo data could be solved by a geo lookup service and ipaddress
+
+		final String siteid = request.getParameter("site");
 		try {
-			final int zoneid = Integer.valueOf(request.getParameter("zone"));
-			pm.setZone(ZoneDataCache.instance.get(zoneid));
-		} catch (final NumberFormatException e) {
-			throw new RequestException(ERROR_CODE.E906, "Zone");
+			pm.setSite(SiteDataCache.instance.get(siteid));
 		} catch (final EmptyCacheException e) {
-			throw new RequestException(ERROR_CODE.E908, e.getMessage());
+			throw new RequestException(e.getMessage());
 		}
 
-		try {
-			final int adid = Integer.valueOf(request.getParameter("ad"));
-			pm.setVideoad(VideoAdDataCache.instance.get(adid));
-		} catch (final NumberFormatException e) {
-			throw new RequestException(ERROR_CODE.E909, "VideoAd");
-		} catch (final EmptyCacheException e1) {
-			throw new RequestException(ERROR_CODE.E909, e1.getMessage());
-		}
-
-		try {
-			pm.setWebsite(WebsiteDataCache.instance.get(pm.getZone().getWebsiteId()));
-		} catch (final EmptyCacheException e) {
-			throw new RequestException(ERROR_CODE.E907, e.getMessage());
-		}
-
-		pm.setDomain(checkValue(request.getParameter("domain"), ERROR_CODE.E906, "Domain"));
-		pm.setH(checkValue(request.getParameter("h"), ERROR_CODE.E906, "Height"));
-		pm.setW(checkValue(request.getParameter("w"), ERROR_CODE.E906, "Width"));
-		pm.setMimes(convertMimes(request.getParameter("mimes")));
-		pm.setPage(checkValue(request.getParameter("page"), pm.getDomain()));
-		pm.setStartdelay(Integer.valueOf(checkValue(request.getParameter("sd"), "0")));
-		pm.setPublisherid(checkValue(request.getParameter("pub"), String.valueOf(pm.getZone().getWebsiteId())));
-		pm.setProtocols(convertProtocolValues(request.getParameter("prot")));
+		// pm.setDomain(checkValue(request.getParameter("domain"), ERROR_CODE.E906, "Domain"));
+		// pm.setH(checkValue(request.getParameter("h"), ERROR_CODE.E906, "Height"));
+		// pm.setW(checkValue(request.getParameter("w"), ERROR_CODE.E906, "Width"));
+		// pm.setMimes(convertMimes(request.getParameter("mimes")));
+		// pm.setPage(checkValue(request.getParameter("page"), pm.getDomain()));
+		// pm.setStartdelay(Integer.valueOf(checkValue(request.getParameter("sd"), "0")));
+		// pm.setProtocols(convertProtocolValues(request.getParameter("prot")));
 
 		return pm;
 	}
 
-	private static String checkValue(final String toCheck, final ERROR_CODE code, final String message) throws RequestException {
-		if (StringUtils.isEmpty(toCheck)) {
-			throw new RequestException(code, message);
-		}
-		return toCheck;
-	}
+	// private static String checkValue(final String toCheck, final ERROR_CODE code, final String message) throws RequestException {
+	// if (StringUtils.isEmpty(toCheck)) {
+	// throw new RequestException(code, message);
+	// }
+	// return toCheck;
+	// }
 
-	private static String checkValue(final String toCheck, final String alternative) {
-		if (StringUtils.isEmpty(toCheck)) {
-			return alternative;
-		}
-		return toCheck;
-	}
+	// private static String checkValue(final String toCheck, final String alternative) {
+	// if (StringUtils.isEmpty(toCheck)) {
+	// return alternative;
+	// }
+	// return toCheck;
+	// }
 
-	private static List<Integer> convertProtocolValues(final String sProtocols) {
-		final List<Integer> protocols = new ArrayList<>();
-		if (!StringUtils.isEmpty(sProtocols)) {
-			final String[] protos = sProtocols.split(",");
-			for (final String protocol : protos) {
-				protocols.add(VideoBidResponseProtocol.convertFromString(protocol));
-			}
-		} else {
-			protocols.add(VideoBidResponseProtocol.VAST_3_0.getValue());
-		}
-		return protocols;
-	}
+	// private static List<Integer> convertProtocolValues(final String sProtocols) {
+	// final List<Integer> protocols = new ArrayList<>();
+	// if (!StringUtils.isEmpty(sProtocols)) {
+	// final String[] protos = sProtocols.split(",");
+	// for (final String protocol : protos) {
+	// protocols.add(VideoBidResponseProtocol.convertFromString(protocol));
+	// }
+	// } else {
+	// protocols.add(VideoBidResponseProtocol.VAST_3_0.getValue());
+	// }
+	// return protocols;
+	// }
 
-	private static List<String> convertMimes(final String mimeTypes) {
-		List<String> mimeTypeList = new ArrayList<>();
-		if (!StringUtils.isEmpty(mimeTypes)) {
-			mimeTypeList = Arrays.asList(mimeTypes.split(","));
-			return mimeTypeList;
-		}
-		// default
-		mimeTypeList.add("video/mp4");
-		return mimeTypeList;
-	}
+	// private static List<String> convertMimes(final String mimeTypes) {
+	// List<String> mimeTypeList = new ArrayList<>();
+	// if (!StringUtils.isEmpty(mimeTypes)) {
+	// mimeTypeList = Arrays.asList(mimeTypes.split(","));
+	// return mimeTypeList;
+	// }
+	// // default
+	// mimeTypeList.add("video/mp4");
+	// return mimeTypeList;
+	// }
 }
