154d5c083c8bb05369397f94d11332cb60c2de47
schmer <schmer@ad-tech-group.com>
2017-10-27 15:49:47 +0200

    Using data from ParamValue (example coding).

--- open-ssp-parent/core/src/main/java/com/atg/openssp/core/exchange/BidRequestBuilder.java
+++ open-ssp-parent/core/src/main/java/com/atg/openssp/core/exchange/BidRequestBuilder.java
@@ -7,7 +7,6 @@ import openrtb.bidrequest.model.Device;
 import openrtb.bidrequest.model.Gender;
 import openrtb.bidrequest.model.Geo;
 import openrtb.bidrequest.model.Impression;
-import openrtb.bidrequest.model.Site;
 import openrtb.bidrequest.model.User;
 import openrtb.bidrequest.model.Video;
 import openrtb.tables.VideoBidResponseProtocol;
@@ -27,13 +26,11 @@ public class BidRequestBuilder {
 	 */
 	public static BidRequest build(final SessionAgent agent) {
 
-		// TODO: fill with true data
-		final BidRequest bidRequest = new BidRequest.Builder().setId("9876").setSite(new Site.Builder().setId("1234").setDomain("domain").addCat("1").addCat("3").setName(
-		        "mySiteObject").setPage("page").build()).setDevice(new Device.Builder().setGeo(new Geo.Builder().setCity("Hamburg").setCountry("DEU").setLat(53.563452f).setLon(
-		                9.925742f).setZip("22761").build()).build()).addImp(new Impression.Builder().setId("45").setVideo(new Video.Builder().addMime(
-		                        "application/x-shockwave-flash").setH(400).setW(600).setMaxduration(100).setMinduration(30).addProtocol(VideoBidResponseProtocol.VAST_2_0
-		                                .getValue()).setStartdelay(1).build()).build()).setUser(new User.Builder().setBuyeruid("HHcFrt-76Gh4aPl").setGender(Gender.MALE).setId("99")
-		                                        .setYob(1981).build()).build();
+		final BidRequest bidRequest = new BidRequest.Builder().setId(agent.getRequestid()).setSite(agent.getParamValues().getSite()).setDevice(new Device.Builder().setGeo(
+		        new Geo.Builder().setCity("Hamburg").setCountry("DEU").setLat(53.563452f).setLon(9.925742f).setZip("22761").build()).build()).addImp(new Impression.Builder().setId(
+		                "1").setVideo(new Video.Builder().addMime("application/x-shockwave-flash").setH(400).setW(600).setMaxduration(100).setMinduration(30).addProtocol(
+		                        VideoBidResponseProtocol.VAST_2_0.getValue()).setStartdelay(1).build()).build()).setUser(new User.Builder().setBuyeruid("HHcFrt-76Gh4aPl")
+		                                .setGender(Gender.MALE).setId("99").setYob(1981).build()).build();
 
 		return bidRequest;
 	}
