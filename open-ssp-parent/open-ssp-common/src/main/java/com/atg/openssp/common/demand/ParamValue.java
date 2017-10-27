1553b659680558888e1e6a19f0612e24b251081f
schmer <schmer@ad-tech-group.com>
2017-10-27 15:48:31 +0200

    Reduced for new (example) datastructure.

--- open-ssp-parent/open-ssp-common/src/main/java/com/atg/openssp/common/demand/ParamValue.java
+++ open-ssp-parent/open-ssp-common/src/main/java/com/atg/openssp/common/demand/ParamValue.java
@@ -1,10 +1,7 @@
 package com.atg.openssp.common.demand;
 
-import java.util.List;
-
-import com.atg.openssp.common.dto.VideoAd;
-import com.atg.openssp.common.dto.Website;
-import com.atg.openssp.common.dto.Zone;
+import openrtb.bidrequest.model.Publisher;
+import openrtb.bidrequest.model.Site;
 
 /**
  * Optimized for handling VideoAd impressions with the behaviour of very individual requirements of the tag handler which binds to the SSP.
@@ -16,110 +13,95 @@ import com.atg.openssp.common.dto.Zone;
  */
 public class ParamValue {
 
-	private Zone zone;
-	private Website website;
-	private VideoAd videoad;
-	private String w;
-	private String h;
-	private String publisherid;
-	private List<String> mimes;
-	private String domain;
-	private String page;
-	private List<Integer> protocols;
-	private int startdelay;
+	// private Zone zone;
+	private Site site;
+	// private VideoAd videoad;
+	// private String w;
+	// private String h;
+	// private List<String> mimes;
+	// private String domain;
+	// private String page;
+	// private List<Integer> protocols;
+	// private int startdelay;
 
-	public Zone getZone() {
-		return zone;
+	private Publisher publisher;
+
+	public Site getSite() {
+		return site;
 	}
 
-	public void setZone(final Zone zone) {
-		this.zone = zone;
+	public void setSite(final Site site) {
+		this.site = site;
 	}
 
-	public Website getWebsite() {
-		return website;
+	// public String getW() {
+	// return w;
+	// }
+
+	// public void setW(final String w) {
+	// this.w = w;
+	// }
+
+	// public String getH() {
+	// return h;
+	// }
+
+	// public void setH(final String h) {
+	// this.h = h;
+	// }
+
+	// public List<String> getMimes() {
+	// return mimes;
+	// }
+
+	// public void setMimes(final List<String> mimes) {
+	// this.mimes = mimes;
+	// }
+
+	// public String getDomain() {
+	// return domain;
+	// }
+
+	// public void setDomain(final String domain) {
+	// this.domain = domain;
+	// }
+
+	// public String getPage() {
+	// return page;
+	// }
+
+	// public void setPage(final String page) {
+	// this.page = page;
+	// }
+
+	// public void setProtocols(final List<Integer> list) {
+	// protocols = list;
+	//
+	// }
+
+	// public List<Integer> getProtocols() {
+	// return protocols;
+	// }
+
+	// public void setStartdelay(final int startdelay) {
+	// this.startdelay = startdelay;
+	// }
+
+	// public int getStartdelay() {
+	// return startdelay;
+	// }
+
+	public Publisher getPublisher() {
+		return publisher;
 	}
 
-	public void setWebsite(final Website website) {
-		this.website = website;
-	}
-
-	public VideoAd getVideoad() {
-		return videoad;
-	}
-
-	public void setVideoad(final VideoAd videad) {
-		videoad = videad;
-	}
-
-	public String getW() {
-		return w;
-	}
-
-	public void setW(final String w) {
-		this.w = w;
-	}
-
-	public String getH() {
-		return h;
-	}
-
-	public void setH(final String h) {
-		this.h = h;
-	}
-
-	public String getPublisherid() {
-		return publisherid;
-	}
-
-	public void setPublisherid(final String publisherid) {
-		this.publisherid = publisherid;
-	}
-
-	public List<String> getMimes() {
-		return mimes;
-	}
-
-	public void setMimes(final List<String> mimes) {
-		this.mimes = mimes;
-	}
-
-	public String getDomain() {
-		return domain;
-	}
-
-	public void setDomain(final String domain) {
-		this.domain = domain;
-	}
-
-	public String getPage() {
-		return page;
-	}
-
-	public void setPage(final String page) {
-		this.page = page;
-	}
-
-	public void setProtocols(final List<Integer> list) {
-		protocols = list;
-
-	}
-
-	public List<Integer> getProtocols() {
-		return protocols;
-	}
-
-	public void setStartdelay(final int startdelay) {
-		this.startdelay = startdelay;
-	}
-
-	public int getStartdelay() {
-		return startdelay;
+	public void setPublisher(final Publisher publisher) {
+		this.publisher = publisher;
 	}
 
 	@Override
 	public String toString() {
-		return "ParamValue [zone=" + zone + ", website=" + website + ", videoad=" + videoad + ", w=" + w + ", h=" + h + ", publisherid=" + publisherid + ", mimes=" + mimes + ", domain=" + domain + ", page=" + page + ", protocols=" + protocols + ", startdelay=" + startdelay + "]";
+		return String.format("ParamValue [site=%s, publisher=%s]", site, publisher);
 	}
 
 }
