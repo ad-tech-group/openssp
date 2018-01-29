80985539e8aaa8c028bd092185db3d9112805c2a
schmer <schmer@ad-tech-group.com>
2018-01-29 12:44:18 +0100

    New exception.

---
+++ open-ssp-parent/open-ssp-common/src/main/java/com/atg/openssp/common/exception/NotifyingException.java
@@ -0,0 +1,15 @@
+package com.atg.openssp.common.exception;
+
+/**
+ * @author André Schmer
+ *
+ */
+public class NotifyingException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NotifyingException(final String message) {
+		super(message);
+	}
+
+}
