e70727c61ff2577e9984b1ba2c95f5ba0625b43f
Brian Sorensen <brian.sorensen@freestar.io>
2018-03-02 14:10:05 -0700

    merge clean-up

--- open-ssp-parent/open-ssp-common/src/main/java/com/atg/openssp/common/configuration/GlobalContext.java
+++ open-ssp-parent/open-ssp-common/src/main/java/com/atg/openssp/common/configuration/GlobalContext.java
@@ -8,6 +8,10 @@ public class GlobalContext extends Context {
 
 	private static long executionTimeout;
 	private static float drawModeration;
+	private static String entryValidatorHandlerForBannerClass;
+	private static String entryValidatorHandlerForVideoClass;
+	private static String adserverBrokerHandlerClass;
+    private static String bidRequestBuilderHandlerClass;
 
 	public static void refreshContext() {
 		// default value is 500 milliseconds
@@ -17,6 +21,11 @@ public class GlobalContext extends Context {
 		// default value is 0.3
 		drawModeration = Float.parseFloat(ContextCache.instance.get(ContextProperties.DRAW_MODERATION) != null ? ContextCache.instance.get(
 		        ContextProperties.DRAW_MODERATION) : "0.3");
+
+		entryValidatorHandlerForBannerClass = ContextCache.instance.get(ContextProperties.VALIDATOR_HANDLER_FOR_BANNER_CLASS);
+		entryValidatorHandlerForVideoClass = ContextCache.instance.get(ContextProperties.VALIDATOR_HANDLER_FOR_VIDEO_CLASS);
+		adserverBrokerHandlerClass = ContextCache.instance.get(ContextProperties.ADSERVER_BROKER_HANDLER_CLASS);
+        bidRequestBuilderHandlerClass = ContextCache.instance.get(ContextProperties.BUILD_REQUEST_BUILDER_HANDLER_CLASS);
 	}
 
 	/**
@@ -41,4 +50,19 @@ public class GlobalContext extends Context {
 		return drawModeration;
 	}
 
+    public static String getEntryValidatorHandlerForVideoClass() {
+        return entryValidatorHandlerForVideoClass;
+    }
+
+	public static String getEntryValidatorHandlerForBannerClass() {
+		return entryValidatorHandlerForBannerClass;
+	}
+
+	public static String getAdserverBrokerHandlerClass() {
+        return adserverBrokerHandlerClass;
+    }
+
+    public static String getBidRequestBuilderHandlerClass() {
+        return bidRequestBuilderHandlerClass;
+    }
 }
